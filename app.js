3e1b1430d4a1810dfb0d8afc756d4b1ae929f2e1
Bavindu Kaviranga <bavindukaviranga2020@gmail.com>
2025-04-22 22:44:27 +0530

    array indexof

---
+++ app.js
@@ -0,0 +1,73 @@
+
+
+// function gessN(){
+//     let systemNumber=Math.floor(Math.random()*10+1);
+
+// if(systemNumber=document.getElementById("intGess").value){
+//    console.log("Congradulations... you won");
+   
+// } 
+// elseif(systemNumber>document.getElementById("intGess").value ){
+//     document.getElementById("c").value(console.log("too low"));
+// }
+// elseif(){
+//     document.getElementById("c").value(console.log("too high"));
+// }
+// }
+
+
+// let num=document.getElementById("intGess").value;
+// console.log(systemNumber);
+
+
+let customerList=[
+    "saman",
+    "kamal",
+    "sunil",
+    "vimal",
+    "ranil",
+    "sirimal",
+    "somapala",
+    "sumanapala",
+    {
+        numbers:[10,20,30,40,50,60,70]
+    },
+];
+console.log(customerList[8].numbers[2]);
+
+// let index=customerList.indexOf("ranil");
+// console.log(customerList[index]="sss");
+// console.log(customerList);
+
+// console.log(customerList.includes("somapala"));
+
+// let letters=[
+//     "A","B","D","C","Z","G","P"
+// ];
+// console.log(letters.sort());
+
+// let numbers=[
+//     "1","2","0","9","6"
+// ]
+// console.log(numbers.sort());
+let customerList1=[
+    "saman",
+    "kamal",
+    "sunil",
+    "vimal",
+    "ranil",
+    "sirimal",
+    "somapala",
+    "sumanapala",
+    {
+        numbers:[10,20,30,
+            {
+                letters :[
+                    "A","B","C","D"
+                ]
+            }
+            ,40,50,60,70]
+    },
+];
+console.log(customerList1[8].numbers[3].letters[3]);
+
