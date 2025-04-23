6bffbe1237011f5677688d34cbbf503e8ced7d15
Bavindu Kaviranga <bavindukaviranga2020@gmail.com>
2025-04-23 11:27:11 +0530

    responsive

--- app.js
+++ app.js
@@ -20,20 +20,20 @@
 // console.log(systemNumber);
 
 
-let customerList=[
-    "saman",
-    "kamal",
-    "sunil",
-    "vimal",
-    "ranil",
-    "sirimal",
-    "somapala",
-    "sumanapala",
-    {
-        numbers:[10,20,30,40,50,60,70]
-    },
-];
-console.log(customerList[8].numbers[2]);
+// let customerList=[
+//     "saman",
+//     "kamal",
+//     "sunil",
+//     "vimal",
+//     "ranil",
+//     "sirimal",
+//     "somapala",
+//     "sumanapala",
+//     {
+//         numbers:[10,20,30,40,50,60,70]
+//     },
+// ];
+// console.log(customerList[8].numbers[2]);
 
 // let index=customerList.indexOf("ranil");
 // console.log(customerList[index]="sss");
@@ -50,24 +50,32 @@ console.log(customerList[8].numbers[2]);
 //     "1","2","0","9","6"
 // ]
 // console.log(numbers.sort());
-let customerList1=[
-    "saman",
-    "kamal",
-    "sunil",
-    "vimal",
-    "ranil",
-    "sirimal",
-    "somapala",
-    "sumanapala",
-    {
-        numbers:[10,20,30,
-            {
-                letters :[
-                    "A","B","C","D"
-                ]
-            }
-            ,40,50,60,70]
-    },
-];
-console.log(customerList1[8].numbers[3].letters[3]);
+// let customerList1=[
+//     "saman",
+//     "kamal",
+//     "sunil",
+//     "vimal",
+//     "ranil",
+//     "sirimal",
+//     "somapala",
+//     "sumanapala",
+//     {
+//         numbers:[10,20,30,
+//             {
+//                 letters :[
+//                     "A","B","C","D"
+//                 ]
+//             }
+//             ,40,50,60,70]
+//     },
+// ];
+// console.log(customerList1[8].numbers[3].letters[3]);
 
+// function show(){
+//     Swal.fire({
+//         icon: "error",
+//         title: "Oops...",
+//         text: "Something went wrong!",
+//         footer: '<a href="#">Why do I have this issue?</a>'
+//       });
+// }
